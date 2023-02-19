3be81ed0356843a3201c66503e03735690467933
cyborg <henrichukwudi@gmail.com>
2023-02-19 06:12:52 +0100

    changes updated for the repo files

--- RestaurantAPI.Repository/Interface/IRepositoryManager.cs
+++ RestaurantAPI.Repository/Interface/IRepositoryManager.cs
@@ -8,6 +8,20 @@ namespace RestaurantAPI.Repository.Interface
 {
     public interface IRepositoryManager
     {
+<<<<<<< Updated upstream
 
+=======
+        IMenuTableRepo Menu {get; set;}
+        IOrderRepo Order {get; set;}
+        IFoodRepo Food {get; set;}
+        IEmployeeListRepo EmpList {get; set;}
+        IEmployeeRepo Employee {get; set;}
+        IMenuTableRepo EmployeeList {get; set;}
+        IBookingRepo Booking { get; set; }
+        ICustomerRepo Customer { get; set; }
+        IItemRepo Item { get; set; }
+
+        Task SaveAsync();
+>>>>>>> Stashed changes
     }
 }
--- RestaurantAPI.Repository/Service/RepositoryManager.cs
+++ RestaurantAPI.Repository/Service/RepositoryManager.cs
@@ -9,5 +9,16 @@ namespace RestaurantAPI.Repository.Service
 {
     public class RepositoryManager : IRepositoryManager
     {
+        public IMenuTableRepo Menu { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IOrderRepo Order { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IFoodRepo Food { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEmployeeListRepo EmpList { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEmployeeRepo Employee { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IMenuTableRepo EmployeeList { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
